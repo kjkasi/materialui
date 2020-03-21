fdb39a15b2ba793520ef7326d81db814cc4e1aed
root <root@localhost.localdomain>
2020-03-21 16:05:31 +0300

    Небольшая работа над интерфейсом

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -10,28 +12,37 @@ import MenuIcon from '@material-ui/icons/Menu';
 import './app.css';
 
 const App = () => {
-    return (
-      <div>
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" 
-                            color="inherit" 
-                            aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6">
-                    News
-                </Typography>
-                <Button color="inherit">
-                    Login
-                </Button>
-                <Button variant="contained" color="primary">
-                    Hello World
-                </Button> 
-            </Toolbar>
-        </AppBar> 
-      </div>
-    );
-  };
+  return (
+    <Container fixed>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" 
+                      color="inherit" 
+                      aria-label="menu">
+              <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            ACS
+          </Typography>
+          <Button color="inherit">
+            Справочники
+          </Button>
+          <Button color="inherit">
+            Документы
+          </Button>
+          <Button color="inherit">
+            Отчеты
+          </Button>
+          <Button color="inherit">
+            Login
+          </Button>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button> 
+        </Toolbar>
+      </AppBar> 
+    </Container>
+  );
+};
 
-  export default App;
+export default App;
